4592bcf5f53910062bf525c499ee81fe91a93798
Ibrahim Mattus Neto <2496339+IMATTUS@users.noreply.github.com>
2022-04-22 16:00:36 -0300

    Creating Seller class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
@@ -8,7 +11,10 @@ public class Main {
 
 		Department obj = new Department(1, "Books");
 		
+		Seller seller = new Seller(21, "Bob", "Bob@gmail.com",new Date(), 3000.0, obj);
+		
 		System.out.println(obj);
+		System.out.println(seller);
 		
 	}
 
